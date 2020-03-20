e681948ce3a95541eb164d4e16935a0641421997
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-03-20 14:31:07 +0100

    added css on showing products

--- src/components/products/EachProduct.jsx
+++ src/components/products/EachProduct.jsx
@@ -4,26 +4,31 @@ import "../../styles/products.css";
 const EachProduct = ({ images, name, brand, colors, sizes }) => {
   return (
     <div className="product-wrapper">
-      {images.map(image => (
-        <img className="img-shirt" src={image} alt={name} />
-      ))}
+      <div className="container">
+        <img className="img-shirt" src={images[0]} alt={name} />
+        {images.length > 1 && (
+            <div className="overlay-img">
+                <img className="img-shirt" src={images[1]} alt={name} />
+            </div>
+        )}
+        <div className="overlay sizes">
+          <ul>
+            {sizes.map((size, i) => (
+              <li className="list" key={i}>
+                <b>{size}</b>
+              </li>
+            ))}
+          </ul>
+        </div>
+        
+      </div>
       <h3>{name}</h3>
       <h4>{brand}</h4>
-      <div className="colors">
+      {/* <div className="colors">
         <p>
           Colors: <b>{colors}</b>
         </p>
-      </div>
-      <div className="sizes">
-        <p>Sizes:</p>
-        <ul>
-          {sizes.map((size, i) => (
-            <li className="list" key={i}>
-              <b>{size}</b>
-            </li>
-          ))}
-        </ul>
-      </div>
+      </div> */}
     </div>
   );
 };
